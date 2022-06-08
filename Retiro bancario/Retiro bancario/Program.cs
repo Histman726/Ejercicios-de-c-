47f803996da1382c4cd8be796df55804da39b531
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-08 13:56:17 -0500

    Descripcion del reto

--- Retiro bancario/Retiro bancario/Program.cs
+++ Retiro bancario/Retiro bancario/Program.cs
@@ -10,6 +10,16 @@ namespace Retiro_bancario
     {
         static void Main(string[] args)
         {
+            /*
+             1-Realiza un sistema que solicite un NIP de cajero al
+            usuario que sea válido (NIP=3867)si el NIP es inválido el
+            sistema deberá permitir 3 intentos más.
+            2.-Inicia el saldo del usuario en $4,000.00 y muestra en
+            pantalla el menú.
+            3-El menú deberá permitir: Consultar Saldo,Retirar de
+            Efectivo,Consultar Movimientos y Salir.
+             */
+
             int pin = 3867, userPin = 0, intentos = 4, opc = 0;
             decimal userSaldo = 4000, montoRetiro = 0;
             List<string> listaMovimientos = new List<string>();
