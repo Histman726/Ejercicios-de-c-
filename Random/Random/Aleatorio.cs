f371f7251968fe10e92b1e77a012eee955c9553c
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-09 18:31:36 -0500

    Clase aleatorios

---
+++ Random/Random/Aleatorio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_numeros
+{
+    public class Aleatorio
+    {
+        public Random NumAlea { get; set; }
+
+        public Aleatorio()
+        {
+            NumAlea = new Random();
+        }
+        public int GenerarNumero(int min,int max)
+        {
+            if(min < max)
+                return NumAlea.Next(min,max+1);
+            else
+                return 0;
+        }
+        public int[] GenerarNumeroAleotorios(int longitud,int min, int max)
+        {
+            if(longitud < 1)
+            {
+                return null;
+            }
+            else
+            {
+                int[] result = new int[longitud];
+                for(int i = 0; i < result.Length; i++)
+                {
+                    result[i] = GenerarNumero(min, max);
+                }
+                return result;
+            }
+            
+
+        }
+    }
+}
---
+++ Random/Random/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aleatorio a = new Aleatorio();
+
+
+            Console.WriteLine("Genero 20 numeros entre 1 y 20");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(a.GenerarNumero(1, 20));
+            }
+
+            Console.WriteLine("Genero 20 numeros entre 1 y 20 en un array");
+            int[] arr = a.GenerarNumeroAleotorios(5, 1, 20);
+            for (int i = 0; arr != null && i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
