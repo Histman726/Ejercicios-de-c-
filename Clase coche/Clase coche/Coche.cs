1b663f5f3e7b2cbb7132f9ae5b890532c398ad51
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-09 16:21:22 -0500

    Clase coche

---
+++ Clase coche/Clase coche/Coche.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_coche
+{
+    public class Coche
+    {
+        public int ID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public double Km { get; set; }
+        public decimal Precio { get; set; }
+
+        public Coche(int iD, string marca, string modelo, double km, decimal precio)
+        {
+            ID = iD;
+            Marca = marca;
+            Modelo = modelo;
+            Km = km;
+            Precio = precio;
+        }
+
+        public Coche()
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + ID + ", Marca: " + Marca + ", Modelo: " + Modelo + ", Km: " + Km + ", Precio: " + Precio;
+        }
+    }
+}
---
+++ Clase coche/Clase coche/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_coche
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche coche=new Coche(1,"Chevrolet","0152",35,3500);
+
+            Console.WriteLine(coche);
+            Console.ReadKey();
+        }
+    }
+}
