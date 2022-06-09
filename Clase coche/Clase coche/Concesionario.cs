fc6447e283904a0c96b6f694fb0fbf5f9236bca1
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-06-09 17:06:16 -0500

    Clase concesionario

---
+++ Clase coche/Clase coche/Concesionario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_coche
+{
+    public class Concesionario
+    {
+        public Coche[] Coches { get; set; }
+        public int Limite { get; set; }
+        public int NumCoches { get; set; }
+
+        public Concesionario(int limite)
+        {
+            this.Coches=new Coche[limite];
+            this.Limite = limite;
+            this.NumCoches=0;
+        }
+        public void AñadirCoche(Coche c)
+        {
+            if (c != null && this.NumCoches<Coches.Length)
+            {
+                Coches[this.NumCoches] = c;
+                NumCoches++;
+            }
+        }
+        public void MostrarCoche()
+        {
+            for (int i = 0; i < NumCoches; i++)
+            {
+                Console.WriteLine(Coches[i]);
+            }
+        }
+        public void VaciarCoche()
+        {
+            Coches=new Coche[Limite];
+        }
+        public void EliminarCoche(Coche c)
+        {
+            if(c != null && this.NumCoches !=0)
+            {
+                int position=0;
+                for(int i = 0; i < NumCoches; i++)
+                {
+                    if(c.ID == Coches[i].ID)
+                    {
+                        position=i;
+                    }
+                }
+                if(position <= 0)
+                {
+                    Console.WriteLine("El coche no existe");
+                }
+                else
+                {
+                    Coches[position] = null;
+                    for(int i = position; i < NumCoches; i++)
+                    {
+                        Coches[i] = Coches[i + 1];
+                    }
+                    NumCoches--;
+                }
+            }
+        }
+    }
+}
--- Clase coche/Clase coche/Program.cs
+++ Clase coche/Clase coche/Program.cs
@@ -10,10 +10,38 @@ namespace Clase_coche
     {
         static void Main(string[] args)
         {
-            Coche coche=new Coche(1,"Chevrolet","0152",35,3500);
+            Concesionario concesionario = new Concesionario(10);
 
-            Console.WriteLine(coche);
+            Coche c1 = new Coche(1, "BMW", "4", 1000, 12000);
+            Coche c2 = new Coche(2, "Toyota", "auris", 0, 12000);
+            Coche c3 = new Coche(3, "Seat", "Ibiza", 2000, 15000);
+            Coche c4 = new Coche(4, "Ferrari", "rosca", 1000, 20000);
+            Coche c5 = new Coche(5, "Peugeout", "206", 100000, 30000);
+
+            concesionario.AñadirCoche(c1);
+            concesionario.AñadirCoche(c2);
+            concesionario.AñadirCoche(c3);
+            concesionario.AñadirCoche(c4);
+            concesionario.AñadirCoche(c5);
+
+            Console.WriteLine("Todos los coches");
+            concesionario.MostrarCoche();
+
+            concesionario.EliminarCoche(c3);
+            concesionario.EliminarCoche(c1);
+
+            Console.WriteLine("Elimino dos coches");
+            concesionario.MostrarCoche();
+
+            concesionario.AñadirCoche(c3);
+            Console.WriteLine("Añadir un coche mas");
+            concesionario.MostrarCoche();
+
+            Console.WriteLine("vacio");
+            concesionario.VaciarCoche();
+            concesionario.MostrarCoche();
             Console.ReadKey();
+
         }
     }
 }
