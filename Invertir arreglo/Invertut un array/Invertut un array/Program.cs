c3bfe6301a660964e7c202e1c001af68769da475
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-07-16 14:34:10 -0500

    Invertir una arreglo

---
+++ Invertir arreglo/Invertut un array/Invertut un array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invertir_un_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             - Pedir al humano que ingrese un texto.
+             - Obtener la longitud del texto.
+             - Crear un ciclo inverso que almacene las letras pero al revés.
+             - Mostrar el texto invertido.
+             */
+            string texto = string.Empty,textoInvertido=string.Empty;
+            char res = 's';
+
+            do
+            {
+                Console.WriteLine("Escribe una palabra o frase");
+                texto = Console.ReadLine();
+                foreach(var letra in texto)
+                {
+                    textoInvertido = letra + textoInvertido;
+                }
+                Console.WriteLine("Su texto de manera invertida es: " + textoInvertido);
+
+                Console.WriteLine("¿Desea repetir el ciclo?");
+                res = Convert.ToChar(Console.ReadLine());
+            } while (res == 's');
+        }
+    }
+}
