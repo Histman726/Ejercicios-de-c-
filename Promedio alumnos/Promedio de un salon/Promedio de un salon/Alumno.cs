d1b70f9c316b7559966d9d1a836567ef27c9e291
Histman726 <hsandovaltovar@alumno.ipn.mx>
2022-07-21 13:42:44 -0500

    Promedio de un salon

---
+++ Promedio alumnos/Promedio de un salon/Promedio de un salon/Alumno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_de_un_salon
+{
+    class Alumno
+    {
+        private string _nombre;
+        private double _calificacion;
+        private char _aula;
+
+        public Alumno(string nombre,double calificacion,char aula)
+        {
+            if (calificacion > 10)
+                calificacion = 10;
+            else if (calificacion < 0)
+                calificacion = 0;
+            else
+                _calificacion = calificacion;
+            _nombre = nombre;
+            _aula = aula;
+        }
+
+        public string GetNombre()
+        {
+            return _nombre;
+        }
+
+        public double GetCalif()
+        {
+            return _calificacion;
+        }
+
+        public char GetAula()
+        {
+            return _aula;
+        }
+    }
+}
---
+++ Promedio alumnos/Promedio de un salon/Promedio de un salon/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_de_un_salon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la cantidad de alumnos");
+            int alumnos = int.Parse(Console.ReadLine());
+            Alumno[] alum = new Alumno[alumnos];
+            List<Alumno> alumnos1 = new List<Alumno>(alumnos);
+
+            for(int i=0; i < alumnos; i++)
+            {
+                Console.WriteLine("Ingresa el nombre del alumno" + (i + 1));
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingresa la calificacion del alumno" + (i + 1));
+                double calif = double.Parse( Console.ReadLine());
+                Console.WriteLine("Ingresa el aula del alumno" + (i + 1));
+                char aulaAlumnos = char.Parse( Console.ReadLine());
+                alum[i] = new Alumno(nombre, calif, aulaAlumnos);
+                alumnos1.Add(new Alumno(nombre,calif,aulaAlumnos));
+            }
+
+            double suma = 0,promedio=0;
+            int Salon = 0;
+            Console.WriteLine("De que aula quieres promediar las calificaciones");
+            char aula = char.Parse(Console.ReadLine());
+            for(int i = 0; i < alumnos; i++)
+            {
+                if (aula == alum[i].GetAula())
+                {
+                    suma += alum[i].GetCalif();
+                    Salon++;
+                }
+            }
+            promedio = suma / Salon;
+            Console.WriteLine("El promedio de los alumnos del salon {0} es {1}", aula, promedio);
+            Console.ReadKey();
+        }
+    }
+}
